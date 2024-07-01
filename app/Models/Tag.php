5f5bea46efd3655542ec1bf688d613ff486268c6
Ariana Sanchez <asanchez.mtz@outlook.com>
2024-07-01 17:01:19 -0600

    Cambios slug

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,10 @@ class Tag extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function slug(): Attribute{
+        return Attribute::make(
+            get: fn () => strtolower(str_replace(' ', '-', $this->name))
+        );
+    }
 }
