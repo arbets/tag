75f1662d3fe01c1a661d005bd54b48bb9392bd03
Ariana Sanchez <asanchez.mtz@outlook.com>
2024-07-01 16:29:48 -0600

    Validación

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -8,12 +8,20 @@ use Illuminate\Http\Request;
 class TagController extends Controller
 {
     public function store(Request $request){
+        
+        $request->validate([
+            'name' => 'required'
+        ]);
+
         Tag::create($request->all());
+
         return redirect('/');
     }
 
     public function delete(Tag $tag){
+        
         $tag->delete();
+        
         return redirect('/');
     }
 }
